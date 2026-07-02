9bbdc4404ff4043c6b56d4fef1984a36b4aae75c
Tanmay <tanmayrastogi08@gmail.com>
2026-07-02 20:49:30 +0530

    react install

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import RelatedProducts from '../components/RelatedProducts';
+import { toast } from 'react-toastify';
 
 const Product = () => {
 
@@ -63,7 +64,15 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button onClick={()=>addToCart(productData._id,weights)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <button onClick={() => {
+    if (!weights) {
+      toast.error("Please select a weight");
+      return;
+    }
+
+    addToCart(productData._id, weights);
+    toast.success("Added to cart 🛒");
+  }} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
           <hr className='mt-8 sm:w-4/5'></hr>
           <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
             <p>100% Original product</p>
