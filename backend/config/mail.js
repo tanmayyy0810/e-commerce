4307db95aa2cb1fc0fe60375b17dde6b06232461
Tanmay <tanmayrastogi08@gmail.com>
2026-07-03 15:47:21 +0530

    Configured Brevo SMTP

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,16 +1,13 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
 });
 
 export default transporter;
